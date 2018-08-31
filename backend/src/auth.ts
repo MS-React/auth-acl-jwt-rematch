a0fa8afa507d1cb9826b74825d0b519a08b41892
Ramiro Gonzalez <rgonzalez@makingsense.com>
2018-08-31 17:43:39 -0300

    Fixes middleware to catch required scopes

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -9,14 +9,14 @@ export type res = { status: number; message: string };
 export async function expressAuthentication(request: Request, securityName: string, scopes?: string[]): Promise<res> {
   switch (securityName) {
     case 'jwt':
+      // @TODO: Usar en ves del authorization el x-access-token, va a ser más prolijo
       const token = request.headers.authorization.replace('Bearer ', '');
-      console.log('token?', token);
       const response = new Promise<res>((resolve, reject) => {
         if (!token) reject(new Error('No token provided'));
         jwt.verify(token, constants.JWT.secretKey, function (err: any, decoded: any) {
             if (err) reject(err);
-            if (!scopes.indexOf(decoded.rol)) reject(new Error('JWT does not contain required scope.'));
-            resolve(decoded);
+            if (scopes.indexOf(decoded.rol) > -1) resolve(decoded);
+            reject(new Error('JWT does not contain required scope.'));
         });
       });
       return response;
